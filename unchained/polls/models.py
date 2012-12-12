d4055f1aa7fd4a7f07de4cdd5ee2ded9acf41703
David Saharkhiz <david.saharkhiz@lamppostgroup.com>
2012-12-12 16:13:50 -0500

    added some poll options

--- unchained/polls/models.py
+++ unchained/polls/models.py
@@ -7,9 +7,13 @@ from django.db import models
 class Poll(models.Model):
 	question = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date published')
+	def __unicode__(self):
+		return	self.question
 	
 class Choice(models.Model) :
 	#establishes a many-to-one relationship, each choice is associated with a foreign key poll
 	poll = models.ForeignKey(Poll)
 	choice = models.CharField(max_length=200)
 	votes = models.IntegerField()
+	def __unicode__(self):
+		return	self.choice
